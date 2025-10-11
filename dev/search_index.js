f3933e178309ce4c5d3ecbfb330d93b7560ba5cf
Documenter.jl <documenter@juliadocs.github.io>
2025-10-11 17:59:42 +0000

    build based on 41f4584

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#sansar","page":"Home","title":"sansar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for sansar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
